3a1c18857fa862d32938317dabaebcafe3c8ff3f
unknown <hoangdinhthongbmt@gmail.com>
2024-04-19 23:26:55 +0700

    thong

--- src/GioHang.java
+++ src/GioHang.java
@@ -1,2 +1,13 @@
+import java.util.List;
+
 public class GioHang {
+    private String maGioHang;
+    private List<SanPhamTrongGioHang> danhSachSP;
+    private double tongTien;
+
+    public GioHang() {}
+
+    public List<SanPhamTrongGioHang> getDanhSachSP() {
+        return danhSachSP;
+    }
 }
