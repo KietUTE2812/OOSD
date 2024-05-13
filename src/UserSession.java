68b12e10e11499319f26836ebd8c9bb8e894391f
LeDien763 <lebadien46@gmail.com>
2024-05-14 03:11:25 +0700

    capnhatthongtintaikhoan capquyen

--- src/UserSession.java
+++ src/UserSession.java
@@ -3,7 +3,14 @@ public class UserSession {
     private static UserSession instance;
     private String username;
     private Byte role;
-
+    private String MaNV;
+    private String hoVaTen;
+    private String sDT;
+    private String email;
+    private String diaChi;
+    private String taiKhoan;
+    private String matKhau;
+    private String MaKH;
     private UserSession() {}
 
     public static UserSession getInstance() {
@@ -27,5 +34,69 @@ public class UserSession {
 
     public void setRole(Byte role) {
         this.role = role;
+    }
+
+    public String getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(String maNV) {
+        MaNV = maNV;
+    }
+
+    public String getHoVaTen() {
+        return hoVaTen;
+    }
+
+    public void setHoVaTen(String hoVaTen) {
+        this.hoVaTen = hoVaTen;
+    }
+
+    public String getsDT() {
+        return sDT;
+    }
+
+    public void setsDT(String sDT) {
+        this.sDT = sDT;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getTaiKhoan() {
+        return taiKhoan;
+    }
+
+    public void setTaiKhoan(String taiKhoan) {
+        this.taiKhoan = taiKhoan;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public String getMaKH() {
+        return MaKH;
+    }
+
+    public void setMaKH(String maKH) {
+        MaKH = maKH;
     }
 }
